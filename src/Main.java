443a35d48fa5e28cfd2e97ea202028b9d8230cfd
vojtech.hampl <v.hampl31@gmail.com>
2023-02-19 15:16:46 +0100

    Second commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name;
+        String prijmeni;
+        LocalDate datumNarozeni;
+        int pocetSjednanychSmluv;
+        int prodaneMrkveKg;
+        String sidlo;
+        String spz;
+        Double spotreba;
+        String ipAdresa;
+
+        name = "Honza";
+        prijmeni = "Nejedlý";
+        datumNarozeni = LocalDate.of(1995, 04, 23);
+        pocetSjednanychSmluv = 958;
+        prodaneMrkveKg = 98754;
+        sidlo = "Brno";
+        spz = "6AS 2565";
+        spotreba = 4.5;
+        ipAdresa = "192.168.48.39";
+
+        //Použití proměnné
+        System.out.println("Průměrné množství prodané mrkve na jednu smlouvu je: "+ prodaneMrkveKg/pocetSjednanychSmluv);
     }
 }
